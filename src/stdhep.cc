715e22bf275963d2f9b32949961653d30dd7a951
Sho Uemura <meeg@slac.stanford.edu>
2012-05-29 13:17:38 -0700

    initial commit

---
+++ src/stdhep.cc
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdhep_mcfio.h>
+#include <stdhep.h>
+#include <TFile.h>
+#include <TH2D.h>
+#include <TCanvas.h>
+#include <string.h>
+
+
+int main(int argc,char** argv)
+{
+	char outputname[200];
+	if (argc!=4) 
+	{
+		printf("<input stdhep filename> <output ROOT filename> <number of events>\n");
+		return 1;
+	}
+	int n_events = atoi(argv[3]);
+	printf("Reading %d events from %s\n",n_events,argv[1]);
+	int istream,ilbl;
+	istream=0;
+	int i,j;
+	StdHepXdrReadInit(argv[1],n_events,istream);
+	StdHepXdrRead(&ilbl,istream);
+
+	TFile *f = new TFile(argv[2],"RECREATE");
+
+	TH2D *thetaE = new TH2D("thetaE","E vs. theta",100,0.0,0.2,100,0.0,2.2);
+	TH2D *e_thetaE = new TH2D("e_thetaE","electron E vs. theta",100,0.0,0.2,100,0.0,2.2);
+	TH2D *p_thetaE = new TH2D("p_thetaE","positron E vs. theta",100,0.0,0.2,100,0.0,2.2);
+	TH2D *g_thetaE = new TH2D("g_thetaE","gamma E vs. theta",100,0.0,0.2,100,0.0,2.2);
+
+	double theta;
+	double *phep;
+	char name[100];
+	for (i=0;i<n_events;i++)
+	{
+		StdHepXdrRead(&ilbl,istream);
+		if (ilbl!=1)
+		{
+			printf("End of file\n");
+			break;
+		}
+		for (j = 0; j < hepevt_.nhep; j++)
+		{
+			phep = hepevt_.phep[j];
+			theta = atan2(sqrt(phep[0]*phep[0]+phep[1]*phep[1]),phep[2]);
+			thetaE->Fill(theta,phep[3]);
+			switch (hepevt_.idhep[j])
+			{
+				case 11:
+					e_thetaE->Fill(theta,phep[3]);
+					sprintf(name,"e-");
+					break;
+				case -11:
+					p_thetaE->Fill(theta,phep[3]);
+					sprintf(name,"e+");
+					break;
+				case 22:
+					g_thetaE->Fill(theta,phep[3]);
+					sprintf(name,"g");
+					break;
+			}
+			if (j==0) strcat(name,"r");
+			printf("%s\t%d\t%d\n",name,(int)(phep[3]*1e6),(i+2)*500*20);
+		}
+	}
+	TCanvas *c1 = new TCanvas("c1", "c1",10,10,1000,750);
+	c1->SetLogz();
+	thetaE->Draw("COLZ");
+	strcpy(outputname,argv[2]);
+	c1->SaveAs(strcat(outputname,"_thetaE.png"));
+	e_thetaE->Draw("COLZ");
+	strcpy(outputname,argv[2]);
+	c1->SaveAs(strcat(outputname,"e_thetaE.png"));
+	p_thetaE->Draw("COLZ");
+	strcpy(outputname,argv[2]);
+	c1->SaveAs(strcat(outputname,"p_thetaE.png"));
+	g_thetaE->Draw("COLZ");
+	strcpy(outputname,argv[2]);
+	c1->SaveAs(strcat(outputname,"g_thetaE.png"));
+
+	f->Write();
+	f->Close();
+}
