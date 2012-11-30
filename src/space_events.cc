901a95552f9c5c95eb5c129b917e1e014bd14a52
Sho Uemura <meeg@slac.stanford.edu>
2012-11-30 12:12:31 -0800

    add space_events, add max output files option to merge_poisson

--- src/space_events.cc
+++ src/space_events.cc
@@ -4,9 +4,6 @@
 #include <string.h>
 #include <stdhep_util.hh>
 
-#include <gsl/gsl_rng.h>
-#include <gsl/gsl_randist.h>
-
 #include <unistd.h>
 
 // takes input stdhep files, merges a Poisson-determined number of events per event into a new stdhep file
@@ -15,24 +12,24 @@ int main(int argc,char** argv)
 	int nevhep;             /* The event number */
 	vector<stdhep_entry> new_event;
 
-	double poisson_mu = 1.0;
+	int event_interval = 500;
 	int output_n = 500000;
 	int max_output_files = 0;
 
 	int c;
 
-	while ((c = getopt(argc,argv,"hn:m:N:")) !=-1)
+	while ((c = getopt(argc,argv,"hn:e:N:")) !=-1)
 		switch (c)
 		{
 			case 'h':
 				printf("-h: print this help\n");
-				printf("-m: mean number of events in an event\n");
+				printf("-e: interval between events\n");
 				printf("-N: max number of files to write\n");
 				printf("-n: output events per output file\n");
 				return(0);
 				break;
-			case 'm':
-				poisson_mu = atof(optarg);
+			case 'e':
+				event_interval = atoi(optarg);
 				break;
 			case 'n':
 				output_n = atoi(optarg);
@@ -47,23 +44,13 @@ int main(int argc,char** argv)
 				abort();
 		}
 
-	printf("Mean events per output event %f, output events per output file %d, max output files %d\n",poisson_mu,output_n,max_output_files);
+	printf("Interval between nonempty events %d, output events per output file %d, max output files %d\n",event_interval,output_n,max_output_files);
 
 	if ( argc-optind <2 )
 	{
 		printf("<input stdhep filenames> <output stdhep basename>\n");
 		return 1;
 	}
-
-	//initialize the RNG
-	const gsl_rng_type * T;
-	gsl_rng * r;
-	gsl_rng_env_setup();
-
-	T = gsl_rng_mt19937;
-	r = gsl_rng_alloc (T);
-	gsl_rng_set(r,0);
-
 
 	int n_events;
 	int istream = 0;
@@ -75,16 +62,15 @@ int main(int argc,char** argv)
 	char output_filename[100];
 	int file_n = 0;
 
+	int events_written = 0;
+
 	open_read(argv[optind++],istream);
 	while (max_output_files==0||file_n<max_output_files) {
 		sprintf(output_filename,"%s_%02d.stdhep",output_basename,file_n++);
 		open_write(output_filename,ostream,output_n);
 		for (int nevhep = 0; nevhep < output_n; nevhep++)
 		{
-			int n_merge = gsl_ran_poisson(r,poisson_mu);
-			if (n_merge==0)
-				add_filler_particle(&new_event);
-			for (int i=0;i<n_merge;i++)
+			if (events_written%event_interval==0)
 			{
 				if (!read_next(istream)) {
 					close_read(istream);
@@ -98,12 +84,16 @@ int main(int argc,char** argv)
 						return(0);
 					}
 				}
-
 				read_stdhep(&new_event);
+			}
+			else
+			{
+				add_filler_particle(&new_event);
 			}
 
 			write_stdhep(&new_event,nevhep+1);
 			write_file(ostream);
+			events_written++;
 		}
 		close_write(ostream);
 	}
