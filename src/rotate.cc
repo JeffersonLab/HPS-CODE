7798940ba029e89883a7b2deab0ce920422b1008
Sho Uemura <meeg@slac.stanford.edu>
2012-12-19 12:55:37 -0800

    fix definition of rotation

--- src/rotate.cc
+++ src/rotate.cc
@@ -4,16 +4,16 @@
 #include <string.h>
 #include <stdhep_util.hh>
 
-void rotate_entry(stdhep_entry entry, double theta)
+void rotate_entry(stdhep_entry *entry, double theta)
 {
-	double px = entry.phep[0];
-	double py = entry.phep[1];
-	double vx = entry.vhep[0];
-	double vy = entry.vhep[1];
-	entry.phep[0] = px*cos(theta) - py*sin(theta);
-	entry.phep[1] = px*sin(theta) + py*cos(theta);
-	entry.vhep[0] = vx*cos(theta) - vy*sin(theta);
-	entry.vhep[1] = vx*sin(theta) + vy*cos(theta);
+	double px = entry->phep[0];
+	double pz = entry->phep[2];
+	double vx = entry->vhep[0];
+	double vz = entry->vhep[2];
+	entry->phep[0] = px*cos(theta) - pz*sin(theta);
+	entry->phep[2] = px*sin(theta) + pz*cos(theta);
+	entry->vhep[0] = vx*cos(theta) - vz*sin(theta);
+	entry->vhep[2] = vx*sin(theta) + vz*cos(theta);
 }
 
 // takes input stdhep file, merges a fixed number of events, and writes to a new stdhep file
@@ -24,7 +24,7 @@ int main(int argc,char** argv)
 
 	if (argc!=4) 
 	{
-		printf("<input stdhep filename> <output stdhep filename> <rotation around Z in radians>\n");
+		printf("<input stdhep filename> <output stdhep filename> <rotation around Y in radians>\n");
 		return 1;
 	}
 	int n_events;
@@ -49,7 +49,7 @@ int main(int argc,char** argv)
 		read_stdhep(&new_event);
 
 		for (int i=0;i<new_event.size();i++)
-			rotate_entry(new_event[i],theta);
+			rotate_entry(&(new_event[i]),theta);
 
 		write_stdhep(&new_event,nevhep+1);
 		write_file(ostream);
