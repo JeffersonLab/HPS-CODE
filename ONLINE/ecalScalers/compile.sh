dfd929a027b1de57eac00bdb9b471f5eaef457e5
baltzell <baltzell@gmx.com>
2015-05-22 06:49:25 -0400

    cleanup

--- ONLINE/ecalScalers/compile.sh
+++ ONLINE/ecalScalers/compile.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
 rootcint -f hps_ecal_scalers_Dict.cxx -c -p hps_ecal_scalers.h hps_ecal_scalers_LinkDef.h
-g++ -W -Wall -Wshadow -Wstrict-aliasing -pthread -m64 -I/home/hpsrun/apps/root_v5.34.21.Linux-slc6-amd64-gcc4.4/include -I/home/hpsrun/.root -c hps_ecal_scalers.cxx
-g++ -W -Wall -Wshadow -Wstrict-aliasing -pthread -m64 -I/home/hpsrun/apps/root_v5.34.21.Linux-slc6-amd64-gcc4.4/include -I/home/hpsrun/.root -c hps_ecal_scalers_Dict.cxx
-g++ -W -Wall -Wshadow -Wstrict-aliasing -pthread -m64 -I/home/hpsrun/apps/root_v5.34.21.Linux-slc6-amd64-gcc4.4/include -L/home/hpsrun/apps/root_v5.34.21.Linux-slc6-amd64-gcc4.4/lib -lCore -lCint -lRIO -lNet -lHist -lGraf -lGraf3d -lGpad -lTree -lRint -lPostscript -lMatrix -lPhysics -lMathCore -lThread -pthread -lm -ldl -rdynamic -L/home/hpsrun/apps/root_v5.34.21.Linux-slc6-amd64-gcc4.4/lib -lGui -I/home/hpsrun/.root -o hps_ecal_scalers hps_ecal_scalers.o  hps_ecal_scalers_Dict.o
+
+g++ -W -Wall -Wshadow -Wstrict-aliasing -pthread -m64 -I$ROOTSYS/include \
+    -c hps_ecal_scalers.cxx
+
+g++ -W -Wall -Wshadow -Wstrict-aliasing -pthread -m64 -I$ROOTSYS/include \
+    -c hps_ecal_scalers_Dict.cxx
+
+g++ -W -Wall -Wshadow -Wstrict-aliasing -pthread -m64 -I$ROOTSYS/include \
+    -L$ROOT/lib -lCore -lCint -lRIO -lNet -lHist -lGraf -lGraf3d -lGpad \
+    -lTree -lRint -lPostscript -lMatrix -lPhysics -lMathCore -lThread \
+    -pthread -lm -ldl -rdynamic -L$ROOTSYS/lib -lGui \
+    -o hps_ecal_scalers hps_ecal_scalers.o  hps_ecal_scalers_Dict.o
 
 
