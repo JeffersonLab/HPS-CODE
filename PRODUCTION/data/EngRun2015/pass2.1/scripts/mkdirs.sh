9457d4b27c438b9d1b9ffcabfdbb398c59947bb0
Nathan Baltzell <baltzell@ifarm1102.jlab.org>
2015-09-30 20:12:01 -0400

    fixing oops

---
+++ PRODUCTION/data/EngRun2015/pass2.1/scripts/mkdirs.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+mkdir -p \
+    $1/logs \
+    $1/recon \
+    $1/dst \
+    $1/data_quality/dqm/ \
+    $1/data_quality/recon \
+    $1/skim/pulser \
+    $1/skim/fee \
+    $1/skim/moller \
+    $1/skim/v0 \
+    $1/skim/dst/pulser \
+    $1/skim/dst/fee \
+    $1/skim/dst/moller \
+    $1/skim/dst/v0
+
