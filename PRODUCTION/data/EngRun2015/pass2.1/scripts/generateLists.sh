37e84240f211f170a7bf120adf3aa09a76c00e5b
Nathan Baltzell <baltzell@ifarm1102.jlab.org>
2015-09-30 20:19:24 -0400

    pulling in scripts

---
+++ PRODUCTION/data/EngRun2015/pass2.1/scripts/generateLists.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+if [ -z "$1" ]; then
+    echo Usage:  listfiles passN
+    exit
+fi
+dir=/u/group/hps/production/data/EngRun2015/$1/lists
+rm -f $dir/disk_$1.txt $dir/tape_$1.txt
+touch $dir/disk_$1.txt $dir/tape_$1.txt
+find /mss/hallb/hps/engrun2015/$1 > $dir/tape_$1.txt
+find /work/hallb/hps/data/engrun2015/$1 > $dir/disk_$1.txt
+
