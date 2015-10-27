148656627c3c434ce6d282aefbefa89d082a5ab3
Sho Uemura <meeg@stanford.edu>
2015-10-26 22:09:16 -0400

    collab meeting stuff

--- fitvtx.py
+++ fitvtx.py
@@ -48,7 +48,7 @@ c.SaveAs(sys.argv[1]+"-zvsmass.png")
 profilehist=totalH.ProfileX("profile")
 profilehist.Draw()
 c.SaveAs(sys.argv[1]+"-profile.png")
-masshist=totalH.ProjectionX("proj")
+masshist=totalH.ProjectionX("mass")
 masshist.SetTitle("Radiative vertex mass")
 masshist.Draw()
 c.SaveAs(sys.argv[1]+"-mass.png")
@@ -57,7 +57,30 @@ gStyle.SetOptStat(1111)
 c.SetLogy(1)
 
 #fitfunc = TF1("fitfunc","[0]*exp((x-[1])<[3]?-(x-[1])^2/(2*[2]^2):[3]^2/(2*[2]^2)-[3]*(x-[1])/([2]^2))")
-fitfunc = TF1("fitfunc","[0]*exp( ((x-[1])<[3])*(-(x-[1])^2/(2*[2]^2)) + ((x-[1])>=[3])*(-[3]^2/(2*[2]^2)+[4]*([3]+[1]-x)))")
+fitfunc = TF1("fitfunc","[0]*exp( ((x-[1])<[3])*(-0.5*(x-[1])^2/[2]^2) + ((x-[1])>=[3])*(-0.5*[3]^2/[2]^2-(x-[1]-[3])/[4]))")
+fitfunc.SetParName(0,"Amplitude")
+fitfunc.SetParName(1,"Mean")
+fitfunc.SetParName(2,"Sigma")
+fitfunc.SetParName(3,"Tail Z")
+fitfunc.SetParName(4,"Tail length")
+#fitfunc = TF1("fitfunc","[0]*exp( ((x-[1])<[3])*(-0.5*(x-[1])^2/[2]^2) + ((x-[1])>=[3])*((x-[1])<[3]+[4])*(0.5*(x-[1]-[3])^2*(-[5]+[3]/[2])/[4] - (x-[1]-[3])*[3]/[2] - 0.5*[3]^2/[2]^2) +  ((x-[1])>=[3]+[4])*(0.5*[4]*(-[3]/[2]-[5]) -0.5*[3]^2/[2]^2 -[5]*(x-[1]-[3]-[4])))")
+#fitfunc.SetParLimits(4,0,100)
+
+#x0 = [1]+[3]
+#dx = [4]
+#f(x0) = -0.5*[3]^2/[2]^2
+#f'(x0) = -[3]/[2]
+#f'(x0+dx) = -[5]
+
+#g''=(f'(x0+dx)-f'(x0))/(dx)
+#g' =(x-x0)*(f'(x0+dx)-f'(x0))/(dx)+f'(x0)
+#g  =0.5(x-x0)^2*(f'(x0+dx)-f'(x0))/(dx)+(x-x0)*f'(x0) +f(x0)
+#0.5*(x-[1]-[3])^2*(-[5]+[3]/[2])/[4] - (x-[1]-[3])*[3]/[2] - 0.5*[3]^2/[2]^2
+
+#g(x0+dx)  =0.5+dx*(f'(x0+dx)-f'(x0))+(dx)*f'(x0) +f(x0)
+#g(x0+dx)  =0.5+dx*(f'(x0+dx)+f'(x0))+f(x0)
+#0.5*[4]*(-[3]/[2]-[5]) -0.5*[3]^2/[2]^2
+
 #fitfunc = TF1("fitfunc","[0]*(exp(-0.5*((x-[1])/[2])^2) + (x-[1]>[2])*[3]*exp(-(x-[1])*[4]))")
 #fitfunc = TF1("fitfunc","exp(pol5(0))")
 #fitfunc = TF1("fitfunc","[0]*exp(max(-(x-[1])^2/(2*[2]^2),[3]))")
@@ -78,12 +101,14 @@ lowtails=array.array('d')
 highcutz=array.array('d')
 lowcutz=array.array('d')
 
+zcutmasses=array.array('d')
 zerobackgroundzcut=array.array('d')
 #h1mass=TH1I(
 
 #resid = TH1D("resid","resid",totalH.GetNbinsY(),totalH.GetXaxis().GetXmin(),totalH.GetXaxis().GetXmax())
 binning=4
-for i in range(0,totalH.GetXaxis().GetNbins()-binning+1):
+for i in range(0,totalH.GetXaxis().GetNbins()-binning+2):
+#for i in range(0,totalH.GetXaxis().GetNbins()-binning+2,binning):
     print i
     lowedge = totalH.GetXaxis().GetBinLowEdge(i)
     highedge = totalH.GetXaxis().GetBinUpEdge(i+binning-1)
@@ -95,7 +120,7 @@ for i in range(0,totalH.GetXaxis().GetNbins()-binning+1):
     name="Radiative vertex Z, mass [{}, {}] GeV".format(lowedge,highedge)
     h1d.SetTitle(name)
     print name
-    if (h1d.GetEntries()>10):
+    if (h1d.GetEntries()>100):
         fit=h1d.Fit("gaus","QS")
         peak=fit.Get().Parameter(0)
         mean=fit.Get().Parameter(1)
@@ -116,8 +141,9 @@ for i in range(0,totalH.GetXaxis().GetNbins()-binning+1):
             highcutz.append(mean+3*sigma)
             lowcutz.append(mean-3*sigma)
             #fitfunc.SetParameters(peak,mean,sigma,0.5,0.2);
-            fitfunc.SetParameters(peak,mean,sigma,3*sigma,0.2);
-            fit=h1d.Fit(fitfunc,"LQ","",mean-2*sigma,mean+10*sigma)
+            fitfunc.SetParameters(peak,mean,sigma,3*sigma,5);
+            #fitfunc.SetParameters(peak,mean,sigma,2.5*sigma,sigma,0.3);
+            fit=h1d.Fit(fitfunc,"LSQM","",mean-2*sigma,mean+10*sigma)
 
             #for j in range(1,h1d.GetNbinsX()+1):
                 #resid.SetBinContent(j,h1d.GetBinContent(j) - h1d.GetFunction("fitfunc").Eval(h1d.GetBinCenter(j)));
@@ -126,10 +152,15 @@ for i in range(0,totalH.GetXaxis().GetNbins()-binning+1):
             #c.SaveAs(remainder[0]+"-"+str(i)+"_resid.png")
             #c.SetLogy(1)
 
-            zcut=fitfunc.GetX(0.5*massrange/reslimited_massrange,mean,200)
-            zerobackgroundzcut.append(zcut)
-            h1d.Draw("E")
-            c.SaveAs(remainder[0]+"-"+str(i)+".png")
+            if fit.Get().IsValid() and fit.Get().Parameter(4)<10:
+                zcutmasses.append((highedge+lowedge)/2)
+                zcut=fitfunc.GetX(0.5*massrange/reslimited_massrange/fit.Get().Parameter(4),mean,200)
+		#print zcut
+		#print 
+		#(log([0]/0.5)-0.5*[3]^2/[2]^2)*[4]-(-[1]-[3]) = x
+                zerobackgroundzcut.append(zcut)
+                h1d.Draw("E")
+                c.SaveAs(remainder[0]+"-"+str(i)+".png")
 
 cutmasses=masses[:]
 
@@ -167,28 +198,30 @@ c.SaveAs(sys.argv[1]+"-lowtails.png")
 
 c.SetLogy(0)
 
-masshist.SetTitle("Radiative vertex mass")
-masshist.Draw("")
-c.SaveAs(sys.argv[1]+"-massnorm.png")
+#masshist.SetTitle("Radiative vertex mass")
+#masshist.Draw("")
+#c.SaveAs(sys.argv[1]+"-massnorm.png")
 
-sigmafitfunc = TF1("sigmafitfunc","[0]*x^[1]",0.02,0.08)
-sigmafitfunc.SetParameters(1,-0.5);
 
 sigmagraph=TGraphErrors(len(masses),masses,sigmas,masserrors,sigmaerrors)
 sigmagraph.SetTitle("Radiative vertex sigma vs. mass")
 sigmagraph.SetName("sigma")
 sigmagraph.Write()
 
+sigmafitfunc = TF1("sigmafitfunc","[0]*x^[1]",0.02,0.08)
+sigmafitfunc.SetParameters(1,-0.5);
 
 sigmagraph.Draw("AP")
-sigmagraph.Fit("sigmafitfunc","SR")
+sigmagraph.Fit("sigmafitfunc","","",0.02,0.06)
+#sigmagraph.Fit("pol5","","",0.02,0.08)
 c.SaveAs(sys.argv[1]+"-sigmas.png")
 
-zcutgraph=TGraph(len(masses),masses,zerobackgroundzcut)
+zcutgraph=TGraph(len(zcutmasses),zcutmasses,zerobackgroundzcut)
 zcutgraph.SetTitle("Z cut for 0.5 background events")
 zcutgraph.SetName("zcut")
 zcutgraph.Write()
 zcutgraph.Draw("A*")
+zcutgraph.Fit("pol4")
 c.SaveAs(sys.argv[1]+"-zcut.png")
 
 outfile.Write()
---
+++ overlay.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+import sys, array
+import getopt
+from ROOT import gROOT, TCanvas, TF1, TFile, gStyle, TFormula, TGraph, TGraphErrors, TH1D, TCutG, TH2D
+
+def makeplots(files,path,name,c):
+	c.Clear()
+	color=1
+	for f in files:
+	    h=f.Get(path)
+	    h.SetLineColor(color)
+	    if color==1:
+		    h.Draw("")
+	    else:
+		    h.Draw("same")
+	    color+=1
+	c.SaveAs(sys.argv[1]+"-"+name+".png")
+
+def makedivplots(files,path1,path2,name,c):
+	c.Clear()
+	color=1
+	for f in files:
+	    h1=f.Get(path1)#.Clone()
+	    #h1.SetDirectory(0)
+	    h2=f.Get(path2)
+	    h1.Divide(h2)
+	    h1.SetLineColor(color)
+	    if color==1:
+		    h1.Draw("")
+	    else:
+		    h1.Draw("same")
+	    color+=1
+	c.SaveAs(sys.argv[1]+"-"+name+".png")
+
+def makenormplots(files,path,name,c):
+	c.Clear()
+	color=1
+	for f in files:
+	    h=f.Get(path)#.Clone()
+	    #h.SetDirectory(0)
+	    h.SetLineColor(color)
+	    h.Scale(1/h.Integral())
+	    if color==1:
+		    h.Draw("")
+	    else:
+		    h.Draw("same")
+	    color+=1
+	c.SaveAs(sys.argv[1]+"-"+name+".png")
+
+scale_factor = 1
+lumi_total = 1240
+
+massres_a = 0.032 #sigma = a*mass + b
+massres_b = 0.001 #units of GeV
+
+options, remainder = getopt.gnu_getopt(sys.argv[1:], 'l:h', ['luminosity','help',])
+
+for opt, arg in options:
+    if opt in ('-l', '--luminosity'):
+        lumi = float(arg)
+        scale_factor = lumi/lumi_total
+    elif opt in ('-h', '--help'):
+        print "\nUsage: "+sys.argv[0]+" <output basename> <root files>"
+        print "Arguments: "
+        print "\t-l, --luminosity: luminosity for normalization"
+        print "\n"
+        sys.exit(0)
+
+
+if (len(remainder)!=3):
+        print sys.argv[0]+' <output basename> <root files>'
+        sys.exit()
+
+c = TCanvas("c","c",800,600);
+outfile = TFile(remainder[0]+"-plots.root","RECREATE")
+totalH = None
+files=[]
+keylists=[]
+for filename in remainder[1:]:
+    f=TFile(filename)
+    files.append(f)
+
+makeplots(files,"sigma","sigma",c)
+makeplots(files,"zcut","zcut",c)
+makeplots(files,"mass","mass",c)
+#makenormplots(files,"mass","massnorm",c)
+c.SetLogy(1)
+makedivplots(files,"hightails","mass","hightails",c)
+makedivplots(files,"lowtails","mass","lowtails",c)
+
+    #keylists.append(f.GetListOfKeys())
+    #print filename
+    #print f.GetListOfKeys()
+    #for blah in  f.GetListOfKeys():
+	#    print blah
+    #if totalH is None:
+        #totalH=TH2D(h)
+        #totalH.SetDirectory(outfile)
+    #else:
+        #totalH.Add(h)
+#for key in files[0].GetListOfKeys():
+	#print key
+	#print key.GetName()
+	#files[0].GetObjectUnchecked(key.GetName()).Draw()
+	#	print 
+
+sys.exit(0)
+
