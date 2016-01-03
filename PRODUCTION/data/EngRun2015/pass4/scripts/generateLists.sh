e2734e871e3c7f0c85ab086c6866847bbd911f19
Nathan Baltzell <baltzell@gmx.com>
2016-01-02 19:03:47 -0500

    add comment

--- PRODUCTION/data/EngRun2015/pass4/scripts/generateLists.sh
+++ PRODUCTION/data/EngRun2015/pass4/scripts/generateLists.sh
@@ -7,8 +7,10 @@ scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pass=`echo $scriptDir | awk -F/ '{print$(NF-1)}'`
 
 dir=/u/group/hps/production/data/EngRun2015/$pass/lists
+
 rm -f $dir/disk_$pass.txt $dir/tape_$pass.txt
 touch $dir/disk_$pass.txt $dir/tape_$pass.txt
+
 find /mss/hallb/hps/engrun2015/$pass > $dir/tape_$pass.txt
 find /work/hallb/hps/data/engrun2015/$pass > $dir/disk_$pass.txt
 
