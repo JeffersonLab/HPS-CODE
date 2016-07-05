c7022610248745c3533d3d6e92769ff59b3f5680
Omar Moreno <omoreno1@ucsc.edu>
2016-07-05 16:40:15 -0700

    Check if the mass hypothesis and histogram name are specified. Add vectors to the ntuple used to store results from the toy studies.

--- ANALYSIS/bump-hunter/src/fit/bump_hunter.cxx
+++ ANALYSIS/bump-hunter/src/fit/bump_hunter.cxx
@@ -2,7 +2,7 @@
  *
  * @file bump_hunter.cxx
  * @author Omar Moreno
- *         SLAC National Accelerator Facility
+ *         SLAC National Accelerator Laboratory
  * @date June 22, 2016
  */
 
@@ -42,12 +42,12 @@ int main(int argc, char **argv) {
 
     // Default polynomial order to use to model the background
     int poly_order = 7;
-   
-    // Log fit results
-    bool log_fit = false;
 
     // Run toys
-    bool run_toys = false; 
+    int n_toys = 0; 
+
+    // Log fit results
+    bool log_fit = false;
  
     // Parse all the command line arguments.  If there are no valid command
     // line arguments passed, print the usage and exit the application
@@ -59,13 +59,13 @@ int main(int argc, char **argv) {
         {"name",       required_argument, 0, 'n'}, 
         {"output",     required_argument, 0, 'o'},
         {"poly",       required_argument, 0, 'p'},
-        {"run_toys",   required_argument, 0, 't'},
+        {"toys",   required_argument, 0, 't'},
         {0, 0, 0, 0}
     };
 
     int option_index = 0;
     int option_char; 
-    while ((option_char = getopt_long(argc, argv, "i:hlm:n:o:p:t", long_options, &option_index)) != -1) {
+    while ((option_char = getopt_long(argc, argv, "i:hlm:n:o:p:t:", long_options, &option_index)) != -1) {
         switch(option_char) {
             case 'i': 
                 file_name = optarg;
@@ -88,7 +88,7 @@ int main(int argc, char **argv) {
                 poly_order = atoi(optarg);
                 break;
             case 't':
-                run_toys = true;
+                n_toys = atoi(optarg);
                 break;
             default: 
                 return EXIT_FAILURE;
@@ -103,6 +103,17 @@ int main(int argc, char **argv) {
         return EXIT_FAILURE;
     }
 
+    // Make sure an A' mass hypothesis was specified
+    if (mass_hypo == 0) { 
+        cerr << "[ BUMP HUNTER ]: Please specify a valid mass hypothesis!" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    if (name.empty()) { 
+        cerr << "[ BUMP HUNTER ]: Please specify a histogram to fit." << endl;
+        return EXIT_FAILURE; 
+    }
+
     // Open the ROOT file.  If any problems are encountered, warn the user
     // and exit the application.
     TFile* file = new TFile(file_name.c_str());
@@ -111,11 +122,15 @@ int main(int argc, char **argv) {
         return EXIT_FAILURE;
     }
 
-    TH1* histogram = (TH1*) file->Get(name.c_str()); 
+    TH1* histogram = (TH1*) file->Get(name.c_str());
+    if (histogram == nullptr) { 
+        cerr << "[ BUMP HUNTER ]: Histogram with name " << name << " is invalid." << endl;
+        return EXIT_FAILURE; 
+    } 
 
     // Create a new Bump Hunter instance and set the given properties.
     BumpHunter* bump_hunter = new BumpHunter(poly_order);
-    if (log_fit) bump_hunter->writeResults(); 
+    //if (log_fit) bump_hunter->writeResults(); 
     //bump_hunter->setBounds(.014, .80);  
 
     // Build the string that will be used for the results file name
@@ -124,5 +139,39 @@ int main(int argc, char **argv) {
     }
     
     // Create a new flat ntuple and define the variables it will encapsulate.
-    FlatTupleMaker* tuple = new FlatTupleMaker(output_file, "results"); 
+    FlatTupleMaker* tuple = new FlatTupleMaker(output_file, "results");
+
+    // Add variables to the tuple
+    tuple->addVariable("ap_mass"); 
+    tuple->addVariable("window_size"); 
+
+    /*
+    tuple->addVariable("sig_yield");
+    tuple->addVariable("sig_yield_err");
+    tuple->addVariable("bkg_yield"); 
+    tuple->addVariable("bkg_yield_err");
+    tuple->addVariable("bkg_window_size");  
+    tuple->addVariable("nll");
+    tuple->addVariable("invalid_nll"); 
+    tuple->addVariable("minuit_status");
+    tuple->addVariable("edm");
+    tuple->addVariable("q0"); 
+    tuple->addVariable("p_value"); 
+    tuple->addVariable("upper_limit");
+    */
+
+    tuple->addVector("toy_bkg_yield");  
+    tuple->addVector("toy_bkg_yield_err"); 
+    tuple->addVector("toy_sig_yield");  
+    tuple->addVector("toy_sig_yield_err");  
+    tuple->addVector("toy_upper_limits");
+
+    // Generate and fit toys
+    std::vector<HpsFitResult*> toy_results = bump_hunter->runToys(histogram, n_toys, mass_hypo); 
+
+    tuple->close();
+    file->Close();
+
+    delete bump_hunter; 
+
 }
