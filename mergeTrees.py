49b42965d8ad246775067738491576c8837d42ea
Sho Uemura <meeg@slac.stanford.edu>
2016-07-06 14:49:03 -0700

    loosen tri cuts

--- mergeTrees.py
+++ mergeTrees.py
@@ -10,18 +10,20 @@ def print_usage():
     print '\t-o: apply Omar\'s trident cuts'
     print '\t-v: apply default vertexing cuts'
     print '\t-m: apply default Moller cuts'
-    print '\t-e: use this beam energy'
+    print '\t-e: use this beam energy (default 1.056)'
+    print '\t-c: use this cluster-track deltaT (default 43.0)'
     print '\t-h: this help message'
     print
 
 ebeam=1.056
-triCut = "isPair1&&max(eleMatchChisq,posMatchChisq)<5&&max(abs(eleClT-eleTrkT-43),abs(posClT-posTrkT-43))<4&&abs(eleClT-posClT)<2&&eleClY*posClY<0&&max(eleTrkChisq,posTrkChisq)<50&&uncChisq<10&&eleP<{0}*0.75"
+clusterT = 43.0
+triCut = "isPair1&&max(eleMatchChisq,posMatchChisq)<5&&max(abs(eleClT-eleTrkT-{1}),abs(posClT-posTrkT-{1}))<4&&abs(eleClT-posClT)<2&&eleClY*posClY<0&&max(eleTrkChisq,posTrkChisq)<50&&tarChisq<50&&eleP<{0}*0.75&&tarP<{0}*1.15"
 omarTriCut = "isPair1&&nPos==1&&max(eleMatchChisq,posMatchChisq)<3&&abs(eleClT-posClT)<1.6&&eleClY*posClY<0"
 vertCut = "eleP<{0}*0.85&&posP<{0}*0.85&&elePY*posPY<0&&eleP>{0}*0.05&&posP>{0}*0.05&&uncP<{0}*1.25&&abs(eleClT-posClT)<2&&(eleHasL1&&posHasL1)&&(eleHasL2&&posHasL2)&&max(eleMatchChisq,posMatchChisq)<10&&max(eleTrkChisq,posTrkChisq)<100"
 mollerCut = "topP<{0}*0.85&&botP<{0}*0.85&&uncP>{0}*0.85&&uncP<{0}*1.1&&abs(topTrkT-botTrkT)<5&&abs(uncPX)<{0}*0.02&&abs(uncPY)<{0}*0.01&&vzcChisq<20"
 cut=""
 
-options, remainder = getopt.gnu_getopt(sys.argv[1:], 'tovme:h')
+options, remainder = getopt.gnu_getopt(sys.argv[1:], 'tovme:c:h')
 
 # Parse the command line arguments
 for opt, arg in options:
@@ -35,6 +37,8 @@ for opt, arg in options:
             cut=mollerCut
         if opt=='-e':
             ebeam=float(arg)
+        if opt=='-c':
+            clusterT=float(arg)
         if opt=='-h':
             print_usage()
             sys.exit(0)
@@ -48,7 +52,7 @@ for i in remainder[1:]:
 	chain.Add(i,0)
 print chain.GetEntries()
 outFile = TFile(remainder[0],"RECREATE")
-events = chain.CopyTree(cut.format(ebeam))
+events = chain.CopyTree(cut.format(ebeam,clusterT))
 print events.GetEntries()
 #outFile = TFile(remainder[0],"RECREATE")
 #events.Write()
