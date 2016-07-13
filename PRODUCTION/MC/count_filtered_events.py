f7e05d9e7fe8117d28d56494bef4d96a931228ea
Sho Uemura <meeg@slac.stanford.edu>
2016-07-13 17:59:10 -0400

    simple util to count filtered events and calculate filter efficiency

---
+++ PRODUCTION/MC/count_filtered_events.py
@@ -0,0 +1,19 @@
+#!/bin/env python
+import sys,re,math
+file_count = 0
+input_count = 0
+output_count = 0
+for filename in sys.argv[1:]:
+    with open(filename) as file:
+        for line in file:
+            match = re.match(r'Read (\d+) events, wrote (\d+) of them',line)
+            if match:
+                print line[:-1]
+                #print match.groups()
+                file_count += 1
+                input_count += int(match.group(1))
+                output_count += int(match.group(2))
+                break
+print "totals from {0} files: {1} input events, {2} output events".format(file_count, input_count, output_count)
+print "average per file: {0} input events, {1} output events".format(float(input_count)/file_count, float(output_count)/file_count)
+print "efficiency: {0} pm {1}".format(float(output_count)/input_count,math.sqrt(output_count)/input_count)
