e3c1fb8c80b6e30837596aa0e29044152d65cc7b
HPS (baltzell) <hps@ifarm1102.jlab.org>
2016-07-13 18:08:30 -0400

    set output precision

--- PRODUCTION/MC/count_filtered_events.py
+++ PRODUCTION/MC/count_filtered_events.py
@@ -15,5 +15,5 @@ for filename in sys.argv[1:]:
                 output_count += int(match.group(2))
                 break
 print "totals from {0} files: {1} input events, {2} output events".format(file_count, input_count, output_count)
-print "average per file: {0} input events, {1} output events".format(float(input_count)/file_count, float(output_count)/file_count)
-print "efficiency: {0} pm {1}".format(float(output_count)/input_count,math.sqrt(output_count)/input_count)
+print "average per file: {0:.1f} input events, {1:.1f} output events".format(float(input_count)/file_count, float(output_count)/file_count)
+print "efficiency: {0:.3f} pm {1:.4f}".format(float(output_count)/input_count,math.sqrt(output_count)/input_count)
