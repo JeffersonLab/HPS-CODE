28359d5490f646644835b1610d3b162cdf48933e
HPS (baltzell) <hps@ifarm1102.jlab.org>
2016-07-13 18:09:22 -0400

    tweaks

--- PRODUCTION/MC/count_filtered_events.py
+++ PRODUCTION/MC/count_filtered_events.py
@@ -16,4 +16,4 @@ for filename in sys.argv[1:]:
                 break
 print "totals from {0} files: {1} input events, {2} output events".format(file_count, input_count, output_count)
 print "average per file: {0:.1f} input events, {1:.1f} output events".format(float(input_count)/file_count, float(output_count)/file_count)
-print "efficiency: {0:.3f} pm {1:.4f}".format(float(output_count)/input_count,math.sqrt(output_count)/input_count)
+print "efficiency: {0:.4f} pm {1:.4f}".format(float(output_count)/input_count,math.sqrt(output_count)/input_count)
