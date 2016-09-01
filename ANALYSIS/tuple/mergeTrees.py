3efb7b3d436fce7bc63ca655d8bc81042068548c
Sho Uemura <meeg@slac.stanford.edu>
2016-08-31 19:28:49 -0700

    add options for loose trident cuts

--- ANALYSIS/tuple/mergeTrees.py
+++ ANALYSIS/tuple/mergeTrees.py
@@ -9,8 +9,11 @@ def print_usage():
     print '\t-t: apply default trident cuts'
     print '\t-p: apply pulser trident cuts'
     print '\t-o: apply Omar\'s trident cuts'
+    print '\t-l: apply loose trident cuts'
+    print '\t-V: apply trident cuts without a vertex chisq requirement'
     print '\t-v: apply default vertexing cuts'
     print '\t-m: apply default Moller cuts'
+    print '\t-w: apply default WAB cuts'
     print '\t-e: use this beam energy (default 1.056)'
     print '\t-c: use this cluster-track deltaT (default 43.0)'
     print '\t-h: this help message'
@@ -21,11 +24,14 @@ clusterT = 43.0
 triCut = "isPair1&&max(eleMatchChisq,posMatchChisq)<5&&max(abs(eleClT-eleTrkT-{1}),abs(posClT-posTrkT-{1}))<4&&abs(eleClT-posClT)<2&&eleClY*posClY<0&&max(eleTrkChisq,posTrkChisq)<50&&tarChisq<50&&eleP<{0}*0.75&&tarP<{0}*1.15"
 pulserTriCut = "isPulser&&max(eleMatchChisq,posMatchChisq)<5&&abs(eleClT-posClT)<2&&eleClY*posClY<0&&max(eleTrkChisq,posTrkChisq)<50&&tarChisq<50&&eleP<{0}*0.75&&tarP<{0}*1.15"
 omarTriCut = "isPair1&&nPos==1&&max(eleMatchChisq,posMatchChisq)<3&&abs(eleClT-posClT)<1.6&&eleClY*posClY<0"
+looseTriCut = "isPair1&&max(eleMatchChisq,posMatchChisq)<30&&abs(eleClT-posClT)<2&&eleClY*posClY<0&&max(eleTrkChisq,posTrkChisq)<50"
+noVertTriCut = "isPair1&&max(eleMatchChisq,posMatchChisq)<5&&max(abs(eleClT-eleTrkT-{1}),abs(posClT-posTrkT-{1}))<4&&abs(eleClT-posClT)<2&&eleClY*posClY<0&&max(eleTrkChisq,posTrkChisq)<50&&eleP<{0}*0.75&&tarP<{0}*1.15"
 vertCut = "isPair1&&max(eleMatchChisq,posMatchChisq)<5&&max(abs(eleClT-eleTrkT-{1}),abs(posClT-posTrkT-{1}))<4&&abs(eleClT-posClT)<2&&eleClY*posClY<0&&max(eleTrkChisq,posTrkChisq)<50&&bscChisq<10&&eleP<{0}*0.75&&bscP<{0}*1.15"
 mollerCut = "topP<{0}*0.85&&botP<{0}*0.85&&uncP>{0}*0.85&&uncP<{0}*1.1&&abs(topTrkT-botTrkT)<5&&abs(uncPX)<{0}*0.02&&abs(uncPY)<{0}*0.01&&vzcChisq<20"
+wabCut = "isPair1&&abs(phoClT-eleTrkT-{1})<3"
 cut=""
 
-options, remainder = getopt.gnu_getopt(sys.argv[1:], 'tpovme:c:h')
+options, remainder = getopt.gnu_getopt(sys.argv[1:], 'tpolVvmwe:c:h')
 
 # Parse the command line arguments
 for opt, arg in options:
@@ -35,10 +41,16 @@ for opt, arg in options:
             cut=pulserTriCut
         if opt=='-o':
             cut=omarTriCut
+        if opt=='-l':
+            cut=looseTriCut
+        if opt=='-V':
+            cut=noVertTriCut
         if opt=='-v':
             cut=vertCut
         if opt=='-m':
             cut=mollerCut
+        if opt=='-w':
+            cut=wabCut
         if opt=='-e':
             ebeam=float(arg)
         if opt=='-c':
