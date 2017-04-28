fe160b93e763b5b9e23a03daa385e6b9ae12d396
Omar Moreno <omoreno1@ucsc.edu>
2017-04-27 19:19:22 -0700

    Add residual plot to output.

--- ANALYSIS/bump-hunter/include/utils/FitPrinter.h
+++ ANALYSIS/bump-hunter/include/utils/FitPrinter.h
@@ -12,12 +12,14 @@
 //   ROOT   //
 //----------//
 #include <TCanvas.h>
+#include <TAxis.h>
 
 //------------//
 //   RooFit   //
 //------------//
 #include <RooAddPdf.h>
 #include <RooDataHist.h>
+#include <RooHist.h>
 #include <RooPlot.h>
 #include <RooRealVar.h>
 
@@ -33,12 +35,18 @@ class FitPrinter {
 
         /** Print the resulting fit and save it to the specified output path. */
         void print(RooRealVar* var, RooDataHist* data, RooAddPdf* model, 
-                   std::string range, std::string output_path); 
+                   std::string range, std::string output_path, int n_bins); 
 
     private:
         
         /** ROOT canvas */   
-        TCanvas* _canvas{new TCanvas{"canvas", "canvas", 600, 600}}; 
+        TCanvas* _canvas{new TCanvas{"canvas", "canvas", 900, 800}};
+        
+        /** Pad used to draw fit. */
+        TPad* _main_pad{new TPad{"main_pad", "main_pad", 0.0, 0.2, 1.0, 1.0}}; 
+        
+        /** Pad used to draw residuals. */
+        TPad* _res_pad{new TPad{"main_pad", "main_pad", 0.0, 0.0, 1.0, 0.2}}; 
 
 };  // FitPrinter
 
